ad5a19c0a79c0888f8ebc5fab3d9032350891b77
Matheus_Luiz <matheus.santos@holding.local>
2020-12-10 15:48:36 -0300

    asd

--- CrimesPorPartido.R
+++ CrimesPorPartido.R
@@ -4,26 +4,51 @@ rm(list=ls())
 library("ggplot2")
 library("dplyr")
 library("readxl")
-library("ggridges")
+library("tidyr")
+library("magrittr")
 #Arquivo Conseguido pelo link:
 #https://dados.gov.br/dataset/sistema-nacional-de-estatisticas-de-seguranca-publica/resource/feeae05e-faba-406c-8a4a-512aec91a9d1
 
 
 dados <- read_excel("indicadoressegurancapublicaufjul20.xlsx")
-data.partidos<-read_excel("Excel//Pasta1.xlsx")
 
 
-nomes<-c("UF","TipoCrime","Ano","Mes","Ocorrencia")
+
+nomes<-c("UF","TipoCrime","Ano","Mes","Ocorrencia","Partido")
 names(dados)<-nomes
 
 dados.teste<-dados
 
-
 #Transformando em factor
-data.partidos$UF<-as.factor(data.partidos$UF)
-data.partidos$PARTIDO<-as.factor(data.partidos$PARTIDO)
+summary(dados.teste)
 
-summary(data.partidos)
+#Transformar em factor e o numero de ocorrencias
+dados.teste$TipoCrime<-as.factor(dados.teste$TipoCrime)
+dados.teste$UF<-as.factor(dados.teste$UF)
+dados.teste$Mes<-as.factor(dados.teste$Mes)
+dados.teste$Ocorrencia<-as.numeric(dados.teste$Ocorrencia)
+dados.teste$Partido<-as.factor(dados.teste$Partido)
+
+dados.AC<-filter(dados.teste,UF=="Acre")
+
+dados.AC %>%
+  filter(Ano==2019) %>%
+  group_by(TipoCrime) %>%
+  count(TipoCrime,Quantidade=sum(Ocorrencia)) %>%
+  ggplot()+
+  geom_bar(aes(x=Quantidade,y=TipoCrime, fill = TipoCrime),stat='identity') +
+  ggtitle("Acre")
+
+names(dados.AC)
+
+
+#Coloca as ocorrencias por partido
+dados.AC.Partido<-dados.AC %>%
+  group_by(Ano,Partido) %>%
+    summarise(Ocorrencias=sum(Ocorrencia))
+
+dados.AC.Partido %>% mutate(percent=)
+
 
 
 
